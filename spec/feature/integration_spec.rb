e32b2ed108614b3ad86f4daf33bc445c6bc49366
aaronsanchez01 <aaronsanchez01@tamu.edu>
2022-09-15 17:26:32 -0500

    Added author

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -5,18 +5,27 @@ RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid title inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
-    #fill_in 'Author', with: 'jk rowling'
+    fill_in 'Author', with: 'jk rowling'
     #fill_in 'Price', with: 20
-    #fill_in 'Date', with: ''
+    #fill_in 'Date', with: Date.new(2004, 11, 24)
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
-    #expect(page).to have_content('jk rowling')
-    #expect(page).to have_content(20)
-    #expect(page).to have_content()
+    expect(page).to have_content('jk rowling')
   end
 
-  scenario 'valid all inputs' do
+  scenario 'valid author inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'jk rowling'
+    #fill_in 'Price', with: 20
+    #fill_in 'Date', with: Date.new(2004, 11, 24)
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('jk rowling')
+  end
+
+  scenario 'valid price inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
     fill_in 'Author', with: 'jk rowling'
@@ -24,9 +33,17 @@ RSpec.describe 'Creating a book', type: :feature do
     fill_in 'Date', with: Date.new(2004, 11, 24)
     click_on 'Create Book'
     visit books_path
-    expect(page).to have_content('harry potter')
-    expect(page).to have_content('jk rowling')
     expect(page).to have_content(20)
+  end
+
+  scenario 'valid date inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'jk rowling'
+    fill_in 'Price', with: 20
+    fill_in 'Date', with: Date.new(2004, 11, 24)
+    click_on 'Create Book'
+    visit books_path
     expect(page).to have_content(Date.new(2004, 11, 24))
   end
 end
