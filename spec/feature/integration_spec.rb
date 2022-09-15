2ec22a46ddfdbab6391ea8f2f25671222b08f1e4
aaronsanchez01 <aaronsanchez01@tamu.edu>
2022-09-15 12:55:10 -0500

    Added unit and integration tests.

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -2,11 +2,31 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a book', type: :feature do
-  scenario 'valid inputs' do
+  scenario 'valid title inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
+    #fill_in 'Author', with: 'jk rowling'
+    #fill_in 'Price', with: 20
+    #fill_in 'Date', with: ''
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
+    #expect(page).to have_content('jk rowling')
+    #expect(page).to have_content(20)
+    #expect(page).to have_content()
+  end
+
+  scenario 'valid all inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'jk rowling'
+    fill_in 'Price', with: 20
+    fill_in 'Date', with: Date.new(2004, 11, 24)
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('harry potter')
+    expect(page).to have_content('jk rowling')
+    expect(page).to have_content(20)
+    expect(page).to have_content(Date.new(2004, 11, 24))
   end
 end
